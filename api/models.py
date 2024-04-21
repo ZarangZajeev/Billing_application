d16a4d27223a998b67b040f9672a09739165d399
ZarangZajeev <zarangzajeev@gmail.com>
2024-04-21 22:34:58 +0530

    third commit

--- api/models.py
+++ api/models.py
@@ -6,7 +6,21 @@ class USerProfile(models.Model):
     user=models.OneToOneField(User,on_delete=models.CASCADE)
     shop_name=models.CharField(max_length=200)
     phone=models.PositiveIntegerField()
-    logo=models.ImageField(upload_to='logos',default='default.png')
+    logo=models.ImageField(upload_to='logos',default='images/logo.png',null=True)
 
     def __str__(self):
         return self.shop_name
+    
+class Category(models.Model):
+    name=models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+    
+class Products(models.Model):
+    name=models.CharField(max_length=200)
+    category=models.ForeignKey(Category)
+    price=models.PositiveIntegerField()
+    user=models.ForeignKey(User)
+    description=models.CharField(300)
+    
